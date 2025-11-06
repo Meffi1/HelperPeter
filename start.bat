18918bb6fd095aa0727ab0ab196cbccf03bd3deb
Skipe666 <143454129+Meffi1@users.noreply.github.com>
2025-11-06 19:51:51 +0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo off
+chcp 65001
+cd /d "C:\Users\skrit\Documents\GitHub\desktop-tutorial"
+echo ===============================
+echo    Amy Starting - Запуск...
+echo ===============================
+python peter_power.py
+pause
